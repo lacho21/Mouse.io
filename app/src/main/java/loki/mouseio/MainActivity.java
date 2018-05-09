22f0fc2bb4a3b6eb9844525fc1b32b90a1006f7a
lacho21 <ljcraig4@gmail.com>
2018-05-09 16:03:09 +1000

    Added AccelerCLass

--- app/src/main/java/loki/mouseio/MainActivity.java
+++ app/src/main/java/loki/mouseio/MainActivity.java
@@ -3,6 +3,8 @@ package loki.mouseio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.javacodegeeks.androidaccelerometerexample.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
